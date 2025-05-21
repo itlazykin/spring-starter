41681e393cc2bb7fc024602668b0335755e97430
Denis Lazykin <dzaka89@gmail.com>
2025-05-21 12:37:57 +0300

    Factory method injection

--- src/main/java/com/dmdev/spring/ApplicationRunner.java
+++ src/main/java/com/dmdev/spring/ApplicationRunner.java
@@ -1,6 +1,7 @@
 package com.dmdev.spring;
 
 import com.dmdev.spring.database.pool.ConnectionPool;
+import com.dmdev.spring.database.repository.CompanyRepository;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class ApplicationRunner {
@@ -9,5 +10,7 @@ public class ApplicationRunner {
         var context = new ClassPathXmlApplicationContext("application.xml");
         ConnectionPool connectionPool = context.getBean("p1", ConnectionPool.class);
         System.out.println(connectionPool);
+        CompanyRepository companyRepository = context.getBean("companyRepository", CompanyRepository.class);
+        System.out.println(companyRepository);
     }
 }

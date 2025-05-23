ad0547f36165b0e58cbfa60e1e3d2bb33b93eb27
Denis Lazykin <dzaka89@gmail.com>
2025-05-23 15:05:06 +0300

    @Bean

--- src/main/java/com/dmdev/spring/config/ApplicationConfiguration.java
+++ src/main/java/com/dmdev/spring/config/ApplicationConfiguration.java
@@ -1,7 +1,11 @@
 package com.dmdev.spring.config;
 
+import com.dmdev.spring.database.pool.ConnectionPool;
 import com.dmdev.spring.database.repository.CrudRepository;
+import com.dmdev.spring.database.repository.UserRepository;
 import com.dmdev.web.config.WebConfiguration;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.beans.factory.config.BeanDefinition;
 import org.springframework.context.annotation.*;
 import org.springframework.context.annotation.ComponentScan.Filter;
 import org.springframework.stereotype.Component;
@@ -17,4 +21,28 @@ import org.springframework.stereotype.Component;
                 @Filter(type = FilterType.REGEX, pattern = "com\\..+Repository")
         })
 public class ApplicationConfiguration {
+
+    @Bean("pool2")
+    @Scope(BeanDefinition.SCOPE_SINGLETON)
+    public ConnectionPool pool2(@Value("${db.username}") String username) {
+        return new ConnectionPool("testName", 20);
+    }
+
+    @Bean
+    public ConnectionPool pool3() {
+        return new ConnectionPool("test-pool", 25);
+    }
+
+    @Bean
+    public UserRepository userRepository2(ConnectionPool pool2) {
+        return new UserRepository(pool2);
+    }
+
+    @Bean
+    public UserRepository userRepository3() {
+        ConnectionPool connectionPool1 = pool3();
+        ConnectionPool connectionPool2 = pool3();
+        ConnectionPool connectionPool3 = pool3();
+        return new UserRepository(pool3());
+    }
 }

123adcd7aac51bde83a9d6ea255adf8afce2a0f1
Denis Lazykin <dzaka89@gmail.com>
2025-05-23 15:17:39 +0300

    profiles

--- src/main/java/com/dmdev/web/config/WebConfiguration.java
+++ src/main/java/com/dmdev/web/config/WebConfiguration.java
@@ -1,7 +1,9 @@
 package com.dmdev.web.config;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 
 @Configuration
+@Profile("web")
 public class WebConfiguration {
 }

3ace8781e85db54fc565856d79c339115eb2acdd
Denis Lazykin <dzaka89@gmail.com>
2025-05-26 12:41:03 +0300

    Spring boot - настройка проекта. v2.

--- src/main/java/com/dmdev/spring/config/ApplicationConfiguration.java
+++ src/main/java/com/dmdev/spring/config/ApplicationConfiguration.java
@@ -1,25 +1,14 @@
 package com.dmdev.spring.config;
 
 import com.dmdev.spring.database.pool.ConnectionPool;
-import com.dmdev.spring.database.repository.CrudRepository;
 import com.dmdev.spring.database.repository.UserRepository;
 import com.dmdev.web.config.WebConfiguration;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.beans.factory.config.BeanDefinition;
 import org.springframework.context.annotation.*;
-import org.springframework.context.annotation.ComponentScan.Filter;
-import org.springframework.stereotype.Component;
 
 @Import(WebConfiguration.class)
-@Configuration
-@PropertySource("classpath:application.properties")
-@ComponentScan(basePackages = "com.dmdev.spring",
-        useDefaultFilters = false,
-        includeFilters = {
-                @Filter(type = FilterType.ANNOTATION, value = Component.class),
-                @Filter(type = FilterType.ASSIGNABLE_TYPE, value = CrudRepository.class),
-                @Filter(type = FilterType.REGEX, pattern = "com\\..+Repository")
-        })
+@Configuration(proxyBeanMethods = true)
 public class ApplicationConfiguration {
 
     @Bean("pool2")

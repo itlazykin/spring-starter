4fc8b31495b69d4db73a95977ea216ef1c88c60a
Denis Lazykin <dzaka89@gmail.com>
2025-05-26 13:12:41 +0300

    Lombok added

--- src/main/java/com/dmdev/spring/database/pool/ConnectionPool.java
+++ src/main/java/com/dmdev/spring/database/pool/ConnectionPool.java
@@ -2,20 +2,18 @@ package com.dmdev.spring.database.pool;
 
 import jakarta.annotation.PostConstruct;
 import jakarta.annotation.PreDestroy;
+import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 @Component("pool1")
+@RequiredArgsConstructor
 public class ConnectionPool {
 
+    @Value("${db.username}")
     private final String username;
+    @Value("${db.pool.size}")
     private final Integer poolSize;
-
-    public ConnectionPool(@Value("${db.username}") String username,
-                          @Value("${db.pool.size}") Integer poolSize) {
-        this.username = username;
-        this.poolSize = poolSize;
-    }
 
     @PostConstruct
     private void init() {

bc8858f12c70f37745f7286d33911d91cabc6c35
Denis Lazykin <dzaka89@gmail.com>
2025-05-29 15:41:13 +0300

    Modifying

--- src/test/java/com/dmdev/spring/integration/database/repository/UserRepositoryTest.java
+++ src/test/java/com/dmdev/spring/integration/database/repository/UserRepositoryTest.java
@@ -1,16 +1,31 @@
 package com.dmdev.spring.integration.database.repository;
 
+import com.dmdev.spring.database.entity.Role;
 import com.dmdev.spring.database.repository.UserRepository;
 import com.dmdev.spring.integration.annotation.IT;
 import lombok.RequiredArgsConstructor;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 
+import java.time.LocalDate;
+
+import static org.junit.jupiter.api.Assertions.assertSame;
+
 @IT
 @RequiredArgsConstructor
 class UserRepositoryTest {
 
     private final UserRepository userRepository;
+
+    @Test
+    void checkUpdate() {
+        var ivan = userRepository.getById(1L);
+        assertSame(Role.ADMIN, ivan.getRole());
+        ivan.setBirthDate(LocalDate.now());
+        var resultCount = userRepository.updateRole(Role.USER, 1L, 5L);
+        var theSameIvan = userRepository.getById(1L);
+        assertSame(Role.USER, theSameIvan.getRole());
+    }
 
     @Test
     void checkQueries() {
